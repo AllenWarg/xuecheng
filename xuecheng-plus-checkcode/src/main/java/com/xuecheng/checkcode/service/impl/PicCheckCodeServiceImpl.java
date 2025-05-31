38343d00d66e51d26c6e3f516c259613ef80ff03
H <hgcan163@163.com>
2025-05-31 15:39:21 +0800

    更改pom.xml文件内容，更改自动化部署插件和依赖添加位置为项目api中

--- xuecheng-plus-checkcode/src/main/java/com/xuecheng/checkcode/service/impl/PicCheckCodeServiceImpl.java
+++ xuecheng-plus-checkcode/src/main/java/com/xuecheng/checkcode/service/impl/PicCheckCodeServiceImpl.java
@@ -9,7 +9,6 @@ import com.xuecheng.checkcode.service.CheckCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sun.misc.BASE64Encoder;
-
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
